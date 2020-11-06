0f279dc8604b0b3322ebae9455f90ad26de8945c
Marcelo11114 <dedosearte@hotmail.com>
2020-11-06 11:36:06 -0300

    Tabela ASCII, Codigo ficou sujo, mas ta feito

--- Program.cs
+++ Program.cs
@@ -9,15 +9,25 @@ namespace codigo_ASCII
         {
              Console.Clear();
              
-            string Masc = "000.##";
+            string Masc = "000.##",nchar ;
+            Console.WriteLine("                    Tabela ASCII - Caracteres Imprimíveis");
 
-            for (int i = 0 ; i <=255 ;i++)
+            int l=0, n;
+           Console.WriteLine("|Dec Char Oct  Hex |Dec Char Oct  Hex |Dec Char Oct  Hex |Dec Char Oct  Hex |");
+           Console.WriteLine("|--- ---- ---  --- |--- ---- ---  --- |--- ---- ---  --- |--- ---- ---  --- |");
+            for (int i = 32 ; i <=127 ;i++)
             {
-                //Console.Write($"{i} = ");
-                System.Console.Write($"| {i.ToString(Masc)} - {(char)i} ");
-                if (i%10==0){Console.WriteLine("");}
-
+                Console.Write($"|{i.ToString(Masc)} ");       // decimal
+                System.Console.Write($" {(char)i}   ");      // Caracter
+                nchar= Convert.ToString(i,8);n=Convert.ToInt16(nchar);// ficou feia essa solução
+                Console.Write($"{n.ToString(Masc)}  ");  // oct
+                Console.Write($"{Convert.ToString(i, 16)}  ");// Hex
+                if (l==3){ l=0; Console.WriteLine("|");}
+                  else {l++;}               
             }
+            Console.Write("\nQqr Tecla P/ Sair");
+            Console.ReadKey();
+            Console.Clear();
         }
     }
 }
