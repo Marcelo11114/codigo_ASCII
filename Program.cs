560df616d4ecbfc8128061085f10ffa441d2448e
Marcelo11114 <dedosearte@hotmail.com>
2020-11-06 09:12:04 -0300

    codigo ASCII

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace codigo_ASCII
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             Console.Clear();
+             
+            string Masc = "000.##";
+
+            for (int i = 0 ; i <=255 ;i++)
+            {
+                //Console.Write($"{i} = ");
+                System.Console.Write($"| {i.ToString(Masc)} - {(char)i} ");
+                if (i%10==0){Console.WriteLine("");}
+
+            }
+        }
+    }
+}
